b7cc91dbfe3cc463024526fac7993f29684f0001
ctoq <toq@mailbox.org>
2026-01-22 17:27:40 +0100

    Initial commit
    Created project structure based on 'oledsaver' branch

---
+++ src/clock.c
@@ -0,0 +1,159 @@
+/* Clock Copyright (c) 2026 supertoq
+ * LICENSE: BSD 2-Clause "Simplified"
+ *
+ * gcc $(pkg-config --cflags gtk4 libadwaita-1) -o toqclock main.c io.github.supertoq.clock.gresource.c $(pkg-config --libs gtk4 libadwaita-1)
+ *
+ * main.c
+ * Uhrzeit mit dedizierten Thread für die Zeitmessung unabhängig von der GUI
+ * 2026-01-21
+ *
+ * Please note:
+ * The Use of this code and execution of the applications is at your own risk, I accept no liability!
+ */
+// 
+#define APP_VERSION    "0.0.0"//_0
+#define APP_ID         "io.github.supertoq.clock"
+#define APP_NAME       "Clock"
+#define APP_DOMAINNAME "supertoq-clock"
+/* Fenster in Breite u. Höche (370, 200) */
+#define WIN_WIDTH      420
+#define WIN_HEIGHT     210
+
+
+#include <glib.h>
+#include <gtk/gtk.h>
+#include <adwaita.h>
+#include <time.h>           // für time()
+#include <unistd.h>         // POSIX-Header
+#include <locale.h>         // für setlocale(LC_ALL, "")
+#include <glib/gi18n.h>     // für " _(); "
+
+//#include "icon-gresource.h"
+
+/* Voraussetzung zum kompilieren - libadwaite 1.8 */
+#ifndef ADW_VERSION_1_8
+#error "NOT building against libadwaita 1.8 headers"
+#endif
+
+/* ----- Globale Strukturen ------------------------------------------ */
+typedef struct {               // Für ToastOverlay
+    AdwToastOverlay *toast_overlay;
+} ToastManager;
+static ToastManager toast_manager = { NULL };
+
+
+
+
+
+/* ------------------------------------------------------------------- */
+/* Aktivierungshandler                                                 */
+/* ------------------------------------------------------------------- */
+static void on_activate(GApplication *app, gpointer user_data)
+{
+
+    /* ----- Adwaita-Fenster ----------------------------------------- */
+    AdwApplicationWindow *adw_win = ADW_APPLICATION_WINDOW(adw_application_window_new(GTK_APPLICATION(app)));
+    gtk_window_set_title(GTK_WINDOW(adw_win), APP_NAME);
+    gtk_window_set_default_size(GTK_WINDOW(adw_win), WIN_WIDTH, WIN_HEIGHT);
+    gtk_window_set_resizable(GTK_WINDOW(adw_win), TRUE);
+
+    /* --- NavigationView -------------------------------------------- */
+    AdwNavigationView *nav_view = ADW_NAVIGATION_VIEW(adw_navigation_view_new());
+
+    /* --- ToolbarView ----------------------------------------------- */
+    AdwToolbarView *toolbar_view = ADW_TOOLBAR_VIEW(adw_toolbar_view_new());
+
+    /* --- HeaderBar ------------------------------------------------- */
+    AdwHeaderBar *headerbar = ADW_HEADER_BAR(adw_header_bar_new());
+    GtkWidget *title_label = gtk_label_new(NULL);
+    gtk_widget_add_css_class(title_label, "heading");
+    adw_header_bar_set_title_widget(headerbar, title_label);
+    adw_toolbar_view_add_top_bar(toolbar_view, GTK_WIDGET(headerbar)); // Headerbar in ToolbarView
+
+    /* --- Hauptseite in NavigationView ------------------------------ */
+    AdwNavigationPage *main_page = adw_navigation_page_new(GTK_WIDGET(toolbar_view), APP_NAME);
+    adw_navigation_view_push(nav_view, main_page);
+
+    /* --- ToastOverlay ---------------------------------------------- */
+    toast_manager.toast_overlay = ADW_TOAST_OVERLAY(adw_toast_overlay_new()); // ToastManager in Strukt.
+    adw_toast_overlay_set_child(toast_manager.toast_overlay, GTK_WIDGET(nav_view));
+    /* --- ToastOverlay einfügen  (ToastOverlay-Struktur) ------------ */
+    adw_application_window_set_content(adw_win, GTK_WIDGET(toast_manager.toast_overlay));
+
+     //  Widget Hierarchie:
+     //      ApplicationWindow
+     //      └---ToastOverlay
+     //          └---NavigationView
+     //              └---NavigationPage
+     //                  └---ToolbarView
+     //                      │
+     //                      ├---HeaderBar
+     //                      │   └---Titel
+     //                      ├---Seiteninhalt
+     //                      .
+
+    /* --- Hamburger-Button innerhalb der Headerbar ------------------ */
+    GtkMenuButton *menu_btn = GTK_MENU_BUTTON(gtk_menu_button_new());
+    gtk_menu_button_set_icon_name(menu_btn, "open-menu-symbolic");
+    adw_header_bar_pack_start(headerbar, GTK_WIDGET(menu_btn)); // Link in Headerbar
+
+    /* --- Popover-Menu im Hamburger --------------------------------- */
+    GMenu *menu = g_menu_new();
+//    g_menu_append(menu, _("Einstellungen         "), "app.show-settings");
+//    g_menu_append(menu, _("Infos zu OLED Saver   "), "app.show-about");
+//    GtkPopoverMenu *menu_popover = GTK_POPOVER_MENU(
+//               gtk_popover_menu_new_from_model(G_MENU_MODEL(menu)));
+//    gtk_menu_button_set_popover(menu_btn, GTK_WIDGET(menu_popover));
+
+    /* --- ACTION für Einstellungen u. About-Dialog ------------------ */
+// ...
+
+    /* --- Haupt-BOX ------------------------------------------------- */
+    GtkBox *main_box = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 1));
+    gtk_widget_set_margin_top   (GTK_WIDGET(main_box),    1);   // Rand unterhalb Toolbar
+    gtk_widget_set_margin_bottom(GTK_WIDGET(main_box),   24);   // unterer Rand unteh. der Buttons
+    gtk_widget_set_margin_start (GTK_WIDGET(main_box),   20);   // links
+    gtk_widget_set_margin_end   (GTK_WIDGET(main_box),   20);   // rechts
+    gtk_widget_set_hexpand      (GTK_WIDGET(main_box), TRUE);
+    gtk_widget_set_vexpand      (GTK_WIDGET(main_box), TRUE);
+
+    /* --- Haupt-Box zur ToolbarView hinzufügen ------------------- */
+    adw_toolbar_view_set_content(toolbar_view, GTK_WIDGET(main_box));
+
+// !! weitere Elemente hier ...
+
+    /* --- Hauptfenster im Application-Objekt ablegen (!) --------- */
+    g_object_set_data(G_OBJECT(app), "main-window", GTK_WINDOW(adw_win));
+
+    /* --- Hauptfenster desktop-konform anzeigen ------------------ */
+    gtk_window_present(GTK_WINDOW(adw_win));
+
+    /* ----- Timer starten ---------------------------------------- */
+// !!
+}
+
+/* ------------------------------------------------------------------- */
+/* Anwendungshauptteil                                                 */
+/* ------------------------------------------------------------------- */
+int main(int argc, char **argv)
+{
+    /* Anwendung */
+    g_autoptr(AdwApplication) app =
+                        adw_application_new(APP_ID, G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+
+    /* Localiziation-Setup */
+    const gchar *locale_path = NULL;
+    setlocale(LC_ALL, "");
+    textdomain(APP_DOMAINNAME);
+    bind_textdomain_codeset(APP_DOMAINNAME, "UTF-8");
+    locale_path = "/usr/share/locale";
+    bindtextdomain(APP_DOMAINNAME, locale_path);
+
+    /* Resource-Bundle registrieren */
+//    g_resources_register(resources_get_resource());
+
+    /* Anwendung starten und auf Ereignis warten */
+    return g_application_run(G_APPLICATION(app), argc, argv);
+
+}
